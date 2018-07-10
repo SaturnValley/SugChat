ea4399ed2dea389f85c56afdf225e998b9212ba0
Saturn Valley <atsuro.konishi.6b@stu.hosei.ac.jp>
2018-07-10 10:30:28 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,5 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     chat_rooms_path(resource)
   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_055859) do
+ActiveRecord::Schema.define(version: 2018_07_01_033141) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string "name"
