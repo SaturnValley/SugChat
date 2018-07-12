e19d7a07b4d119c2329f6fa6831c725c1896feff
hk-nyao <shota.ikawa.6e@stu.hosei.ac.jp>
2018-07-13 03:55:02 +0900

    modified database operation and created search room form.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,16 +4,33 @@ class ChatsController < ApplicationController
   # GET /chats
   # GET /chats.json
   def index
-    @search = Chat.ransack(params[:q])
-    @chats = @search.result(distinct: true).page(params[:page]).per(10)
-    @users = User.all
-    @chat_rooms = ChatRoom.all
+    @search_chats = Chat.ransack(params[:q])
+    @chats = @search_chats.result(distinct: true).page(params[:page]).per(10)
+
+    # chat_rooms contains only chat_rooms that removed field is nil by default
+    @search_rooms = ChatRoom.where(removed: nil).ransack(params[:p], search_key: :p)
+    @chat_rooms = @search_rooms.result(distinct: true).page(params[:page]).per(5)
+
+    # users_id contains only users.id who have ever posted chat even once in all @chats
+    @users_id = search_users(@chats)
+    if @users_id.length == 0
+      # if no one has posted a chat, User.all is nothing.
+      @users = User.all
+    else
+      @users = User.where(id: @users_id)
+    end
+
     render :layout => "chats_layout"
   end
 
-  def search
-    @q = Chat.search(search_params)
+  def chat_search
+    @q = Chat.search(search_chat_params)
     @chats = @q.result(distinct: true)
+  end
+
+  def room_search
+    @p = ChatRoom.search(search_room_params)
+    @chat_rooms = @p.result(distinct: true)
   end
 
   # GET /chats/1
@@ -77,12 +94,26 @@ class ChatsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
 
+    def search_users(chats)
+      users_id = []
+      chats.each do |chat|
+        if !users_id.include?(chat.user_id)
+          users_id.push(chat.user_id.to_i)
+        end
+      end
+      return users_id
+    end
+
     def set_chat
       @chat = Chat.find(params[:id])
     end
 
-    def search_params
+    def search_chat_params
       params.require(:q).permit(:chat_room_id, :message)
+    end
+
+    def search_room_params
+      params.require(:p).permit(:name)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
